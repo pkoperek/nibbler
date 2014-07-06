6b4f2092e7ffd8a8a39a2680eb5d50e232089c99
Pawel Koperek <pkoperek@gmail.com>
2014-07-06 16:45:27 +0100

    working main class

---
+++ src/main/scala/EvaluationService.scala
@@ -0,0 +1,8 @@
+/**
+ * User: koperek
+ * Date: 06.07.14
+ * Time: 12:08
+ */
+class EvaluationService {
+
+}
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,17 @@
+/**
+ * User: koperek
+ * Date: 06.07.14
+ * Time: 15:32
+ */
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object Main {
+  def main(args: Array[String]) {
+
+    println("i'm inside!")
+
+  }
+}

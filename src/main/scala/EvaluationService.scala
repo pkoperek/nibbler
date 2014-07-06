a086594286a9d60362a4a0b426d32661ad23ae38
Pawel Koperek <pkoperek@gmail.com>
2014-07-06 20:43:52 +0100

    mvp1

--- src/main/scala/EvaluationService.scala
+++ src/main/scala/EvaluationService.scala
@@ -1,8 +1,22 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkContext, SparkConf}
+
 /**
  * User: koperek
  * Date: 06.07.14
  * Time: 12:08
  */
-class EvaluationService {
+class EvaluationService(timeSeries: RDD[(Long, Double)]) {
 
+  def evaluate(toEvaluate: Double => Double): Double = {
+    var error = 0.0
+
+
+    timeSeries.count()
+
+    //    val result: Double = Math.abs(deltaX / deltaY) - Math.abs(dfdy_val / dfdx_val)
+    //    error += Math.log(1 + Math.abs(result))
+    //
+    //    return error
+  }
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,14 +4,49 @@
  * Time: 15:32
  */
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
+def splitToTimestampAndValue(input: String): (Long, Double) = {
+  val splittedStrings = input.split(",")
+  val timestampAsString = splittedStrings(0)
+
+  return (1L, splittedStrings(1).toDouble)
+}
+
+def evaluate(timestampValues: RDD[(Long, Double)]): Double = {
+  val evaluationService = new EvaluationService(timestampValues)
+
+  evaluationService.evaluate({
+    x => x * 2
+  })
+}
+
 object Main {
+
+  val applicationName = "TestApp"
+
   def main(args: Array[String]) {
 
-    println("i'm inside!")
+    validate(args)
 
+    val masterUrl = args(0)
+    val inputFilePath = args(1)
+    val conf = new SparkConf().setAppName(applicationName).setMaster(masterUrl)
+    val sparkContext = new SparkContext(conf)
+
+    val inputFile: RDD[String] = sparkContext.textFile(inputFilePath)
+    val timestampValues = inputFile.map(splitToTimestampAndValue)
+
+    println("Evaluation result: " + evaluate(timestampValues))
+  }
+
+
+  def validate(args: Array[String]) = {
+    if (args.length != 2) {
+      println("Specify master URL as first parameter! Eg. mesos://149.156.10.32:1237")
+      System.exit(1)
+    }
   }
 }

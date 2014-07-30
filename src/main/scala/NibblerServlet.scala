76bec52cbfd2c5f60e9bfbc92ee9b500bb12aa3b
Pawel Koperek <pkoperek@gmail.com>
2014-07-30 22:32:49 +0100

    remove master parameter from status rq

--- src/main/scala/NibblerServlet.scala
+++ src/main/scala/NibblerServlet.scala
@@ -39,16 +39,9 @@ class FunctionNode(val functionName: String, val operands: Seq[FunctionNode]) {
 class NibblerServlet(sparkContext: SparkContext) extends ScalatraServlet {
 
   get("/status") {
-    // http://149.156.10.32:9198/status?masterUrl=mesos%3A%2F%2Fzk%3A%2F%2Fmaster%3A2181%2Fmesos
-    val masterUrl = params.get("masterUrl")
+    val filteredValues: Array[Int] = sparkContext.parallelize(1 to 10000).filter(_ < 10).collect()
 
-    if (masterUrl.isDefined) {
-      val filteredValues: Array[Int] = sparkContext.parallelize(1 to 10000).filter( _ < 10 ).collect()
-
-      "Test query result: " + filteredValues.mkString(",") + "\nParameters used: " + params
-    } else {
-      "Master URL not specified!"
-    }
+    "Test query result: " + filteredValues.mkString(",") + "\nParameters used: " + params
   }
 
   post("/evaluate") {

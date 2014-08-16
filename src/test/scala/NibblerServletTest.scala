b2b71a5f4a31f930c9c528d5ddbd5792adbd4bd4
Pawel Koperek <pkoperek@gmail.com>
2014-08-16 17:50:16 +0100

    not working servlet tests

---
+++ src/test/scala/NibblerServletTest.scala
@@ -0,0 +1,55 @@
+import org.apache.spark.SparkContext
+import org.eclipse.jetty.server.ServerConnector
+import org.eclipse.jetty.server.nio.SelectChannelConnector
+import org.junit.Ignore
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.scalatest.FunSuiteLike
+import org.scalatest.junit.JUnitRunner
+import org.scalatra.test.scalatest.ScalatraSuite
+
+@Ignore
+@RunWith(classOf[JUnitRunner])
+class NibblerServletTest extends ScalatraSuite with FunSuiteLike {
+
+  val sparkContext = Mockito.mock(classOf[SparkContext])
+
+  addServlet(classOf[NibblerServlet], "/*")
+
+  override def baseUrl: String =
+    server.getConnectors collectFirst {
+      case conn: SelectChannelConnector =>
+        val host = Option(conn.getHost) getOrElse "localhost"
+        val port = conn.getLocalPort
+        val url = "http://%s:%d".format(host, port)
+        println("Using url: " + url)
+        url
+    } getOrElse sys.error("couldn't find a matching connector")
+
+  test("get is unsupported in evaluate") {
+    get("/evaluate") {
+      status should equal (405)
+    }
+  }
+
+  test("should evaluate test file") {
+    val jsonRequest =
+      """
+      {
+        "inputFile": "test.txt",
+        "function":
+        {
+           "function": "sin",
+           "operands": [{
+               "function": "1.0"
+           }]
+        }
+      }
+      """
+
+    post("/evaluate") {
+      status should equal (200)
+    }
+  }
+
+}

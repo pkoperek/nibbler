144d315e92ece0a2efc6170abdcf40f601e193aa
Pawel Koperek <pkoperek@gmail.com>
2014-08-18 23:10:08 +0100

    differential quotients

--- src/main/scala/NumericalDifferentiator.scala
+++ src/main/scala/NumericalDifferentiator.scala
@@ -4,7 +4,7 @@ import org.apache.spark.rdd.RDD
 import scala.collection.mutable.ListBuffer
 
 trait NumericalDifferentiator {
-  def partialDerivative(input: RDD[Seq[Double]]): RDD[Seq[Double]]
+  def partialDerivative(input: RDD[Seq[Double]]): RDD[Double]
 }
 
 object NumericalDifferentiator {
@@ -19,7 +19,7 @@ object NumericalDifferentiator {
    * x_n - x_n-1
    */
   private class BackwardNumericalDifferentiator(differentialQuotientDividend: Int, differentialQuotientDivisor: Int) extends NumericalDifferentiator {
-    override def partialDerivative(input: RDD[Seq[Double]]): RDD[Seq[Double]] = {
+    override def partialDerivative(input: RDD[Seq[Double]]): RDD[Double] = {
 
       val minuend: RDD[(Long, Seq[Double])] = input.zipWithIndex().map {
         _.swap
@@ -28,21 +28,15 @@ object NumericalDifferentiator {
         (row._1 - 1, row._2)
       })
 
-      val diff: RDD[Seq[Double]] = minuend.join(subtrahend).map((row: (Long, (Seq[Double], Seq[Double]))) => {
+      val diff: RDD[Double] = minuend.join(subtrahend).map((row: (Long, (Seq[Double], Seq[Double]))) => {
         val minuendRow = row._2._1
         val subtrahendRow = row._2._2
 
-        val buffer = new ListBuffer[Double]
-
-        for (idx <- 0 to minuendRow.size - 1) {
-          buffer += minuendRow(idx) - subtrahendRow(idx)
-        }
-
-        buffer.toList
+        val quotient = (minuendRow(differentialQuotientDividend) - subtrahendRow(differentialQuotientDividend)) / (minuendRow(differentialQuotientDivisor) - subtrahendRow(differentialQuotientDivisor))
+        quotient
       })
 
       diff
-
     }
   }
 
@@ -50,7 +44,7 @@ object NumericalDifferentiator {
    * x_n - x_n-2
    */
   private class CentralNumericalDifferentiator(differentialQuotientDividend: Int, differentialQuotientDivisor: Int) extends NumericalDifferentiator {
-    override def partialDerivative(input: RDD[Seq[Double]]): RDD[Seq[Double]] = {
+    override def partialDerivative(input: RDD[Seq[Double]]): RDD[Double] = {
 
       val minuend: RDD[(Long, Seq[Double])] = input.zipWithIndex().map {
         _.swap
@@ -59,21 +53,15 @@ object NumericalDifferentiator {
         (row._1 - 2, row._2)
       })
 
-      val diff: RDD[Seq[Double]] = minuend.join(subtrahend).map((row: (Long, (Seq[Double], Seq[Double]))) => {
+      val diff: RDD[Double] = minuend.join(subtrahend).map((row: (Long, (Seq[Double], Seq[Double]))) => {
         val minuendRow = row._2._1
         val subtrahendRow = row._2._2
 
-        val buffer = new ListBuffer[Double]
-
-        for (idx <- 0 to minuendRow.size - 1) {
-          buffer += minuendRow(idx) - subtrahendRow(idx)
-        }
-
-        buffer.toList
+        val quotient = (minuendRow(differentialQuotientDividend) - subtrahendRow(differentialQuotientDividend)) / (minuendRow(differentialQuotientDivisor) - subtrahendRow(differentialQuotientDivisor))
+        quotient
       })
 
       diff
-
     }
   }
 

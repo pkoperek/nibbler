21d261f407ae491f2b86c4d8ffc5de0fb95f620f
Pawel Koperek <pawelk@blinkbox.com>
2014-08-19 17:16:54 +0100

    first failing tc for numerical diff

---
+++ src/test/scala/NumericalDifferentiatorTest.scala
@@ -0,0 +1,16 @@
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import spray.json._
+import org.scalatest.Matchers
+import org.scalatest.FunSuite
+import org.scalatest.mock.MockitoSugar
+import org.mockito.Mockito.verify
+
+@RunWith(classOf[JUnitRunner])
+class NumericalDifferentiatorTest extends FunSuite with Matchers with MockitoSugar  {
+
+  test("first test case") {
+    fail("failing on purpose!")
+  }
+
+}

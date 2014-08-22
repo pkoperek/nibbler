f8bb837ad043c7613f46c49f518c5c894047dc12
Pawel Koperek <pawelk@blinkbox.com>
2014-08-22 15:07:09 +0100

    pair generator

--- src/test/scala/PairGeneratorTest.scala
+++ src/test/scala/PairGeneratorTest.scala
@@ -14,10 +14,23 @@ class PairGeneratorTest extends FunSuite with ShouldMatchers {
     pairs should be('empty)
   }
 
-  test("should generate single pair for 1") {
+  test("should generate empty sequence for 1") {
     val pairs = generator.generatePairs(1)
 
-    pairs should equal((0, 1))
+    pairs should be('empty)
   }
 
+  test("should generate single pair for 2") {
+    val pairs = generator.generatePairs(2)
+
+    pairs should contain ((0, 1))
+    pairs should have length (1)
+  }
+
+  test("should generate 3 pairs for 3") {
+    val pairs = generator.generatePairs(3)
+
+    pairs should (contain ((0, 1)) and contain ((0, 2)) and contain ((1, 2)))
+    pairs should have length (3)
+  }
 }

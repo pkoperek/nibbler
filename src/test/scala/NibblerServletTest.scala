4c558c6105d8715a56036f51f5382f736c59ea39
Pawel Koperek <pkoperek@gmail.com>
2014-08-26 21:43:24 +0100

    initialize context before each test case

--- src/test/scala/NibblerServletTest.scala
+++ src/test/scala/NibblerServletTest.scala
@@ -1,19 +1,26 @@
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 import org.junit.runner.RunWith
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.mock.MockitoSugar
 import org.scalatra.test.scalatest.ScalatraSuite
 
 @RunWith(classOf[JUnitRunner])
-class NibblerServletTest extends ScalatraSuite with FunSuite with MockitoSugar {
+class NibblerServletTest extends ScalatraSuite with FunSuite with MockitoSugar with BeforeAndAfterEach {
 
   private val configuration = new SparkConf().setAppName("test").setMaster("local")
-  private val sparkContext = new SparkContext(configuration)
+  private var sparkContext: SparkContext = null
 
-  addServlet(new NibblerServlet(sparkContext), "/*")
+  override protected def beforeEach(): Unit = {
+    sparkContext = new SparkContext(configuration)
+    addServlet(new NibblerServlet(sparkContext), "/*")
+  }
+
+  override protected def afterEach(): Unit = {
+    sparkContext.stop()
+  }
 
   test("should return status") {
     get("/status") {

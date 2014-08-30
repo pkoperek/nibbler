55db935d4a5d375e409732f2dbcfcb4840440879
Pawel Koperek <pkoperek@gmail.com>
2014-08-30 21:50:40 +0100

    extracted error calculation function

--- src/main/scala/FunctionErrorEvaluator.scala
+++ src/main/scala/FunctionErrorEvaluator.scala
@@ -1,36 +1,51 @@
+import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
 
 class FunctionErrorEvaluator(differentiatorType: String) extends Serializable {
 
+  private val errorCalculationFunction = new ErrorCalculationFunction
   private val pairGenerator = new PairGenerator
 
-  def evaluate(input: RDD[Seq[Double]], functionDeserialized: Function) = {
+  def evaluate(input: RDD[Seq[Double]], functionDeserialized: Function): Double = {
     val variablePairs = pairGenerator.generatePairs(2)
-    val results = new StringBuilder
+    var error = Double.MinValue
 
     for (pair <- variablePairs) {
-      val df_dx = functionDeserialized.differentiate("var_" + pair._1)
-      val df_dy = functionDeserialized.differentiate("var_" + pair._2)
+      val symbolicallyDifferentiated = symbolicDifferentiation(input, functionDeserialized, pair)
+      val numericallyDifferentiated = numericalDifferentiation(input, pair)
 
-      val df_dx_evaluated = input.map(df_dx.evaluate).zipWithIndex().map(reverse)
-      val df_dy_evaluated = input.map(df_dy.evaluate).zipWithIndex().map(reverse)
+      val pairingError = errorCalculationFunction.calculateError(symbolicallyDifferentiated, numericallyDifferentiated)
 
-      val differentiator = NumericalDifferentiator(differentiatorType, pair._1, pair._2)
-
-      val numericallyDifferentiated = differentiator.partialDerivative(input).zipWithIndex().map(reverse)
-
-      val result: String = "Results: " +
-        " dfdx cnt: " + df_dx_evaluated.count() +
-        " dfdy cnt: " + df_dy_evaluated.count() +
-        " numdiff cnt: " + numericallyDifferentiated.count()
-
-      println("Partial result: " + result)
-
-      results.append(result)
+      if (error < pairingError) {
+        error = pairingError
+      }
     }
 
-    results.toString()
+    error
+  }
 
+  private def symbolicDifferentiation(input: RDD[Seq[Double]], function: Function, pair: (Int, Int)): RDD[(Long, Double)] = {
+    val df_dx = function.differentiate("var_" + pair._1)
+    val df_dy = function.differentiate("var_" + pair._2)
+
+    val df_dx_evaluated: RDD[(Long, Double)] = input.map(df_dx.evaluate).zipWithIndex().map(reverse)
+    val df_dy_evaluated: RDD[(Long, Double)] = input.map(df_dy.evaluate).zipWithIndex().map(reverse)
+
+    df_dy_evaluated.join(df_dx_evaluated).map(divide)
+  }
+
+  private def divide(row: (Long, (Double, Double))) = {
+    (row._1, row._2._1 / row._2._2)
+  }
+
+  private def numericalDifferentiation(input: RDD[Seq[Double]], pair: (Int, Int)): RDD[(Long, Double)] = {
+    val differentiator = NumericalDifferentiator(differentiatorType, pair._1, pair._2)
+    val numericallyDifferentiated = differentiator.partialDerivative(input).zipWithIndex().map(reverse).map(incrementIdx)
+    numericallyDifferentiated
+  }
+
+  private def incrementIdx(row: (Long, Double)): (Long, Double) = {
+    (row._1 + 1, row._2)
   }
 
   private def reverse(toReverse: (Double, Long)) = toReverse.swap
---
+++ src/test/scala/FunctionErrorEvaluatorTest.scala
@@ -0,0 +1,42 @@
+import Function.{plus, node, sin}
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.mock.MockitoSugar
+
+@RunWith(classOf[JUnitRunner])
+class FunctionErrorEvaluatorTest extends FunSuite with MockitoSugar with ShouldMatchers with SparkContextAware {
+
+  test("evaluates sin(x)") {
+    // Given
+    val function = f(sin(plus(variable(0), variable(1))))
+
+    val input = List(
+      List(10.0, 0.1),
+      List(11.0, 0.2)
+    )
+
+    // When
+    val error = evaluate(function, input)
+
+    // Then
+    error should be(-2.30258 plusOrMinus 0.00001)
+  }
+
+  private def evaluate(function: Function, input: Seq[Seq[Double]]): Double = {
+    new FunctionErrorEvaluator("backward").evaluate(
+      sparkContext.parallelize(input),
+      function
+    )
+  }
+
+  private def variable(idx: Int): FunctionNode = {
+    node("var_" + idx)
+  }
+
+  private def f(tree: FunctionNode): Function = {
+    new Function(tree)
+  }
+
+}

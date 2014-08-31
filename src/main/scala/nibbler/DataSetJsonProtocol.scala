2e6d1e0570b23f720ba8b4d9e30a26976636fe14
Pawel Koperek <pkoperek@gmail.com>
2014-08-31 20:12:17 +0100

    data set serialization to json

---
+++ src/main/scala/nibbler/DataSetJsonProtocol.scala
@@ -0,0 +1,20 @@
+package nibbler
+
+import spray.json._
+
+object DataSetJsonProtocol extends DefaultJsonProtocol {
+
+  implicit object DataSetJsonFormat extends RootJsonFormat[DataSet] {
+    def write(dataSet: DataSet) = {
+      JsObject(
+        "numberOfRows" -> JsNumber(dataSet.getNumberOfRows),
+        "numberOfColumns" -> JsNumber(dataSet.getNumberOfColumns)
+      )
+    }
+
+    override def read(json: JsValue): DataSet = {
+      throw new UnsupportedOperationException
+    }
+  }
+
+}

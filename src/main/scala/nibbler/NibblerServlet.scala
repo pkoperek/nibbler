d9b2fb108c5ee487d556e7235f05447ea1a576fb
Pawel Koperek <pkoperek@gmail.com>
2014-09-17 23:11:31 +0100

    remove caching

--- src/main/scala/nibbler/NibblerServlet.scala
+++ src/main/scala/nibbler/NibblerServlet.scala
@@ -29,7 +29,7 @@ class NibblerServlet(sparkContextService: SparkContextService) extends ScalatraS
     val requestAsJson = request.body.parseJson.asJsObject
 
     val inputFile = getValue(requestAsJson, "inputFile")
-    val input = parse(inputFile).cache()
+    val input = parse(inputFile)
 
     val function = getValue(requestAsJson, "function")
     val functionDeserializeed = parseFunction(function)

66bd1756840d9488d5fcfc1c4dd4ab57a0460e58
Pawel Koperek <pkoperek@gmail.com>
2014-09-18 22:26:40 +0100

    combine into single stage

--- src/main/scala/nibbler/ErrorCalculationFunction.scala
+++ src/main/scala/nibbler/ErrorCalculationFunction.scala
@@ -6,23 +6,7 @@ import org.apache.spark.rdd.RDD
 class ErrorCalculationFunction extends Serializable {
 
   private def minus(row: (Long, (Double, Double))) = {
-    (row._1, row._2._1 - row._2._2)
-  }
-
-  private def setIndexTo_1(row: (Long, Double)) = {
-    (1L, row._2)
-  }
-
-  private def abs(row: (Long, Double)) = {
-    (row._1, Math.abs(row._2))
-  }
-
-  private def log(row: (Long, Double)) = {
-    (row._1, Math.log(row._2))
-  }
-
-  private def add_1(row: (Long, Double)) = {
-    (row._1, row._2 + 1.0)
+    (1L, Math.log(Math.abs(row._2._1 - row._2._2) + 1.0))
   }
 
   private def add(left: (Long, Double), right: (Long, Double)) = {
@@ -32,10 +16,6 @@ class ErrorCalculationFunction extends Serializable {
   def calculateError(symbolicallyDifferentiated: RDD[(Long, Double)], numericallyDifferentiated: RDD[(Long, Double)]): Double = {
     val (count, sumOfRowErrors) = numericallyDifferentiated.join(symbolicallyDifferentiated)
       .map(minus)
-      .map(setIndexTo_1)
-      .map(abs)
-      .map(add_1)
-      .map(log)
       .reduce(add)
 
     -sumOfRowErrors / count

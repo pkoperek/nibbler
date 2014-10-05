0da3e339bfe9bf521f630b1182ef9adbaf78ae7f
Pawel Koperek <pkoperek@gmail.com>
2014-10-05 17:46:20 +0100

    ignore unit tests

--- src/test/scala/nibbler/DataSetJsonProtocolTest.scala
+++ src/test/scala/nibbler/DataSetJsonProtocolTest.scala
@@ -12,7 +12,7 @@ class DataSetJsonProtocolTest extends FunSuite with MockitoSugar with ShouldMatc
   test("serializes data set to json without rdd") {
     // Given
     val rdd = mock[RDD[Seq[Double]]]
-    val dataSet = new DataSet(3, 14, rdd)
+    val dataSet = new DataSet((3, 14), rdd, Map[(Int, Int), RDD[(Long, Double)]]())
 
     // When
     val dataSetAsJson: String = dataSet.toJson.toString()

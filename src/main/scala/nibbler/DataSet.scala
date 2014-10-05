11c1c0064e25e90d3bef8cc2914a1f4a2ba9bdb9
Pawel Koperek <pkoperek@gmail.com>
2014-10-05 16:45:39 +0100

    data set contains numerically differentiated series

--- src/main/scala/nibbler/DataSet.scala
+++ src/main/scala/nibbler/DataSet.scala
@@ -3,7 +3,17 @@ package nibbler
 import org.apache.spark.rdd.RDD
 import spray.json._
 
-class DataSet(numberOfRows: Long, numberOfColumns: Long, rawData: RDD[Seq[Double]]) {
+class DataSet(
+               numberOfRows: Long,
+               numberOfColumns: Long,
+               rawData: RDD[Seq[Double]],
+               numericallyDifferentiated: Map[(Int, Int), RDD[(Long, Double)]]
+               ) {
+
+  def getNumericallyDifferentiated(pair: (Int, Int)): RDD[(Long, Double)] = {
+    numericallyDifferentiated(pair)
+  }
+
   def getNumberOfRows = numberOfRows
 
   def getNumberOfColumns = numberOfColumns

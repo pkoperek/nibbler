2f4e9b59dd20582a2f327cd73323653ece358890
Pawel Koperek <pkoperek@gmail.com>
2014-10-05 16:53:13 +0100

    use persisted numerically differentiated sets

--- src/main/scala/nibbler/FunctionErrorEvaluator.scala
+++ src/main/scala/nibbler/FunctionErrorEvaluator.scala
@@ -7,13 +7,13 @@ class FunctionErrorEvaluator(differentiatorType: String) extends Serializable {
   private val errorCalculationFunction = new ErrorCalculationFunction
   private val pairGenerator = new PairGenerator
 
-  def evaluate(input: RDD[Seq[Double]], functionDeserialized: Function): Double = {
-    val variablePairs = pairGenerator.generatePairs(2)
+  def evaluate(input: DataSet, functionDeserialized: Function): Double = {
+    val variablePairs = pairGenerator.generatePairs(input.getNumberOfColumns)
     var error = Double.MinValue
 
     for (pair <- variablePairs) {
       val symbolicallyDifferentiated = symbolicDifferentiation(input, functionDeserialized, pair)
-      val numericallyDifferentiated = numericalDifferentiation(input, pair)
+      val numericallyDifferentiated = input.getNumericallyDifferentiated(pair)
 
       val pairingError = errorCalculationFunction.calculateError(symbolicallyDifferentiated, numericallyDifferentiated)
 
@@ -25,21 +25,11 @@ class FunctionErrorEvaluator(differentiatorType: String) extends Serializable {
     error
   }
 
-  private def symbolicDifferentiation(input: RDD[Seq[Double]], function: Function, pair: (Int, Int)): RDD[(Long, Double)] = {
+  private def symbolicDifferentiation(input: DataSet, function: Function, pair: (Int, Int)): RDD[(Long, Double)] = {
     val df_dx = function.differentiate("var_" + pair._1)
     val df_dy = function.differentiate("var_" + pair._2)
 
-    input.map(x => df_dy.evaluate(x) / df_dx.evaluate(x)).zipWithIndex().map(reverse)
-  }
-
-  private def numericalDifferentiation(input: RDD[Seq[Double]], pair: (Int, Int)): RDD[(Long, Double)] = {
-    val differentiator = NumericalDifferentiator(differentiatorType, pair._1, pair._2)
-    val numericallyDifferentiated = differentiator.partialDerivative(input).zipWithIndex().map(reverse).map(incrementIdx)
-    numericallyDifferentiated
-  }
-
-  private def incrementIdx(row: (Long, Double)): (Long, Double) = {
-    (row._1 + 1, row._2)
+    input.getRawData.map(x => df_dy.evaluate(x) / df_dx.evaluate(x)).zipWithIndex().map(reverse)
   }
 
   private def reverse(toReverse: (Double, Long)) = toReverse.swap

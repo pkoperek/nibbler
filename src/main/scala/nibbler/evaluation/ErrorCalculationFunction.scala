bff0d0ba8060583c16b4b89c5b4dbd1db74b9720
Pawel Koperek <pkoperek@gmail.com>
2015-02-02 23:56:56 +0000

    fix problem with unary operator

--- src/main/scala/nibbler/evaluation/ErrorCalculationFunction.scala
+++ src/main/scala/nibbler/evaluation/ErrorCalculationFunction.scala
@@ -9,13 +9,14 @@ class ErrorCalculationFunction extends Serializable {
     Math.log(Math.abs(row._1 - row._2) + 1.0)
   }
 
-  private def add(left: (Long, Double), right: (Long, Double)) = {
-    (left._1 + right._1, left._2 + right._2)
+  private def add(left: Double, right: Double) = {
+    left + right
   }
 
   def calculateError(symbolicallyDifferentiated: RDD[(Long, Double)], numericallyDifferentiated: RDD[(Long, Double)]): Double = {
     val sumOfRowErrors = numericallyDifferentiated.join(symbolicallyDifferentiated)
       .mapValues(computeError)
+      .map(t => t._2)
       .reduce(add)
 
     -sumOfRowErrors
